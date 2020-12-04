565e32eb736dee6a6609b70e484353fba5aed2f2
bedrantirak <bdrntrk@gmail.com>
2020-12-04 23:17:59 +0300

    Update imageUpload.js

--- insta/src/imageUpload.js
+++ insta/src/imageUpload.js
@@ -1,7 +1,9 @@
 import { Button } from '@material-ui/core'
 import React, { useState } from 'react'
+import firebase from "firebase"
+import {storage, db} from "./firebase"
 
-function ImageUpload() {
+function ImageUpload({username}) {
 
     const [caption, setCaption] = useState('');
     const [progress, setProgress] = useState(0);
@@ -14,8 +16,36 @@ function ImageUpload() {
     };
 
     const handleUpload = () =>{
-        
-    }
+   const uploadTask = storage.ref(`ìmages/${image.name}`).put(image);
+   
+   uploadTask.on(
+       "state_changed",
+       (snapshot) =>{
+          //progress function..
+          const progress = Math.round(
+              (snapshot.bytesTransferred / snapshot.totalBytes)*100
+          );
+          setProgress(progress);
+       },
+       (error) =>{
+           //Error function... 
+           console.log(error);
+           alert(error.message);
+       },
+       ()=>{
+           //complete function... 
+           storage.ref("images").child(image.name).getDownloadURL()
+           .then(url =>{
+              //post image inside db... 
+              db.collection("posts").add({
+                  timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                  caption: caption,
+                  imageUrl: url,
+                  username: username
+                })
+           })
+       }
+   ) }
     return (
         <div>
             
