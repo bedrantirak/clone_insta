1a78aa70264082e263c44c067656c247a6588720
bedrantirak <bdrntrk@gmail.com>
2020-12-05 16:53:44 +0300

    Update App.js

--- insta/src/App.js
+++ insta/src/App.js
@@ -99,15 +99,7 @@ const signIn = (event) =>{
 
   return (
     <div className="App">,
-    {user?.displayName ? (
-         <ImageUpload username={user.displayName} />
-
-    ):(
-      <h3>
-       Sign In for upload image now!
-      </h3>
-    )}
-    
+  
 
 <Modal
      open={openSignIn}
@@ -178,8 +170,7 @@ const signIn = (event) =>{
       src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
       alt="insta_logo"
       />
-    </div>
-    { user ? (
+       { user ? (
            <Button onClick={()=> auth.signOut()}>Logout</Button>
 
     ): (
@@ -188,14 +179,25 @@ const signIn = (event) =>{
       <Button onClick={()=> setOpen(true)}>Sign Up</Button>
       </div>
       )}
+    </div>
+   
 
-    <h1>Clone Insta</h1>
+    <h1>Clone Insta </h1>
       {
         posts.map(({id, post}) =>(
           <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
-   
+     
+     {user?.displayName ? (
+         <ImageUpload username={user.displayName} />
+
+    ):(
+      <h3>
+       Sign In for upload image now!
+      </h3>
+    )}
+    
     
     </div>
   );
